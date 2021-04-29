b1d8140012d1df8592a6234624ac72a253293826
adrianrachman <adrianrachman08@gmail.com>
2021-04-29 09:41:10 +0700

    menyelesaikan pertemuan 12

---
+++ pertemuan12/functions.php
@@ -0,0 +1,71 @@
+<?php
+
+// koneksi ke database
+$conn = mysqli_connect("localhost", "root", "", "phpdasar");
+
+function query($query) {
+	global $conn;
+	$result = mysqli_query($conn, $query);
+	$rows = [];
+	while( $row = mysqli_fetch_assoc($result) ) {
+		$rows[] = $row;
+	}
+	return $rows;
+}
+
+function tambah($data) {
+	global $conn;
+
+	$gambar = htmlspecialchars($data["gambar"]);
+	$namabarang = htmlspecialchars($data["namabarang"]);
+	$harga = htmlspecialchars($data["harga"]);
+	$merk = htmlspecialchars($data["merk"]);
+
+	$query = "INSERT INTO otomotif
+				VALUES
+				('', '$gambar', '$namabarang', '$harga', '$merk')
+			";
+	mysqli_query($conn, $query);
+
+	return mysqli_affected_rows($conn);
+}
+
+function hapus($id) {
+	global $conn;
+	mysqli_query($conn, "DELETE FROM otomotif WHERE id = $id");
+
+	return mysqli_affected_rows($conn);
+}
+
+function ubah($data) {
+	global $conn;
+
+	$id = $data["id"];
+	$gambar = htmlspecialchars($data["gambar"]);
+	$namabarang = htmlspecialchars($data["namabarang"]);
+	$harga = htmlspecialchars($data["harga"]);
+	$merk = htmlspecialchars($data["merk"]);
+
+	$query = "UPDATE otomotif SET
+				gambar = '$gambar',
+				namabarang = '$namabarang',
+				harga = '$harga',
+				merk = '$merk'
+				WHERE id = $id
+			";
+	mysqli_query($conn, $query);
+
+	return mysqli_affected_rows($conn);
+}
+
+function cari($keyword) {
+	$query = "SELECT * FROM otomotif
+				WHERE
+            namabarang LIKE '%$keyword%' OR
+            harga LIKE '%$keyword%' OR
+            merk LIKE '%$keyword%' 
+            ";
+    return query($query);
+}
+
+?>
